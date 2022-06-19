3d6765a89cdba790cb1c334440c6a16b7607efa1
Franco Maksimchuk Castillo <70729879+MaksimchukFL@users.noreply.github.com>
2022-06-19 18:02:14 -0300

    Update EJ2.asm

--- EJ2.asm
+++ EJ2.asm
@@ -4,9 +4,9 @@
 	LIST P=16f84	
 		
 	ORG 0x00
-	MOVF 0x2A,0		;Mueve el contenido de 0x2A a W.
+	MOVF 0x2A,0	;Mueve el contenido de 0x2A a W.
 	ADDWF 0x2B,0	;Suma el contenido de 0x2B con lo que se halla en W, el resultado se mantiene en el registro.
 	ADDWF 0x2C,0	;Se realiza de manera analógica con los demás sumandos.
 	ADDWF 0X2D,0
-	MOVWF 0x0D		;Se almacena el resultado en la dirección especificada.
+	MOVWF 0x0D	;Se almacena el resultado en la dirección especificada.
 	end
